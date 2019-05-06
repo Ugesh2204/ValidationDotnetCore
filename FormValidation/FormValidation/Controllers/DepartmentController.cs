f31d670da73de9ca7b20a1624139186914e56130
ugesh <ugesh.roopchand@hotmail.com>
2019-05-06 11:01:04 +0400

    Department Controller added and Dependency Injection  constructor Added

---
+++ FormValidation/FormValidation/Controllers/DepartmentController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FormValidation.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FormValidation.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        readonly ApplicationDbContext db;
+
+        public DepartmentController(ApplicationDbContext _db)
+        {
+            db = _db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
