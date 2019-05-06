ef17db5dd058104965db562ece45e1f3acc9fd82
ugesh <ugesh.roopchand@hotmail.com>
2019-05-06 10:05:06 +0400

    Initial Commit

---
+++ FormValidation/FormValidation/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FormValidation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
