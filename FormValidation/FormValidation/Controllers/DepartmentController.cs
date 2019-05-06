5fbcda97ad5dec5a639b42666e1404f600b0c080
ugesh <ugesh.roopchand@hotmail.com>
2019-05-06 12:01:38 +0400

    Create Method Added without VALIDATIONS

--- FormValidation/FormValidation/Controllers/DepartmentController.cs
+++ FormValidation/FormValidation/Controllers/DepartmentController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FormValidation.Data;
+using FormValidation.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -21,5 +22,24 @@ namespace FormValidation.Controllers
         {
             return View( await db.Departments.ToListAsync());
         }
+
+
+        //Create Method#
+       [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Department department)
+        {
+            db.Add(department);
+            db.SaveChanges();
+
+            return View(department);
+
+        }
+
     }
 }
