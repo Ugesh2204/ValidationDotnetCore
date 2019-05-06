1eb031028678d476921e57635a1dcd354e8fde73
ugesh <ugesh.roopchand@hotmail.com>
2019-05-06 10:11:49 +0400

    Relational Models Created Code first Approach

---
+++ FormValidation/FormValidation/Models/Department.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormValidation.Models
+{
+    public class Department
+    {
+        [System.ComponentModel.DataAnnotations.Key]
+        public int DepartmentId { get; set; }
+
+        [Required]
+        public string DepartmentName { get; set; }
+
+        public List<Employee> Employees { get; set; }
+    }
+}
---
+++ FormValidation/FormValidation/Models/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormValidation.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+
+        [Required]
+        public string EmployeeName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+        public enum ETitle { Mr = 1, Mrs = 2, Miss = 3 }
+
+        [Required]
+        public double Salary { get; set; }
+
+        [Required]
+        public DateTime HireDate { get; set; }
+
+        [ForeignKey("Department")]
+        public int DepartmentId { get; set; }
+
+        public Department Department { get; set; }
+    }
+}
